b0605d454e62fae4670e460af0c61788491dcfd9
Prajwal-H-Rao <prajwalhhr130@gmail.com>
2024-07-13 19:26:13 +0530

    starter pack

---
+++ src/app/dashboard/layout.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Sidebar from '../ui/dashboard/sidebar/page'
+import Navbar from '../ui/dashboard/navbar/page'
+import styles from '../ui/dashboard/dashboard.module.css'
+const Layout = ({children}) => {
+  return (
+    <div className={styles.container}>
+        <div className={styles.menu}>
+            <Sidebar/>
+        </div>
+        <div className={styles.content}>
+            <Navbar/>
+            {children}
+        </div>
+    </div>
+  )
+}
+
+export default Layout

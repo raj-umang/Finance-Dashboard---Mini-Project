cff21b4e79a09f34ec09a121fe7a06ab9bb4cb72
Prajwal-H-Rao <prajwalhr130@gmail.com>
2024-07-16 16:27:57 +0530

    removed user

--- src/app/lib/models.js
+++ src/app/lib/models.js
@@ -23,29 +23,5 @@ const expenseSchema = new mongoose.Schema(
   { timestamps: true }
 );
 
-const userSchema = new mongoose.Schema(
-  {
-    user_name: {
-      type: String,
-      required: true,
-      unique: true,
-      min: 3,
-      max: 20,
-    },
-    user_mail: {
-      type: String,
-      required: true,
-    },
-    user_password_hash: {
-      type: String,
-      required: true,
-    },
-    img: {
-      type: String,
-    },
-  },
-  { timestamps: true }
-);
 
-export const User = mongoose.models.User || mongoose.model("User", userSchema);
 export const Expense = mongoose.models.Transaction || mongoose.model("Transaction", expenseSchema);

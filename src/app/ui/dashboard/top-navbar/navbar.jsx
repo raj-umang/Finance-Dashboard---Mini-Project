a32505ab08209d0c7bb66bdd41757687c2da396d
dexterzPK <dontwanttolive945@gmail.com>
2024-07-18 16:03:30 +0530

    auth updated

--- src/app/ui/dashboard/top-navbar/navbar.jsx
+++ src/app/ui/dashboard/top-navbar/navbar.jsx
@@ -19,22 +19,7 @@ const Navbar = () => {
     <div className={styles.container}>
       <div className={styles.title}>{pathname.split("/").pop()}</div>
       <div className={styles.menu}>
-        <div className={styles.search}>
-          <MdSearch />
-          <input type="text" placeholder="Search..." className={styles.input} />
-        </div>
-        <div className={styles.icons}>
-          <MdOutlineChat size={20} />
-          <MdNotifications size={20} />
-          <MdPublic size={20} />
-        </div>
         <ul className="flex items-center gap-10">
-          <Link href="/">
-            <li>Home</li>
-          </Link>
-          <Link href="/dashboard">
-            <li>Dashboard</li>
-          </Link>
           {!session ? (
             <>
               <Link href="/login">
